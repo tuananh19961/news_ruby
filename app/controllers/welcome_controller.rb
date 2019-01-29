318baffb2f61c0f99a6e54eed5e11135caf193a9
tuananh29081996 <tuananh29081996@gmail.com>
2019-01-29 21:46:34 +0700

    pagination in home page!

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
     @new_posts = Post.includes(:category).order('posts.id ASC').last(2);
-    @posts = Post.all
+    @posts = Post.all.page(params[:page]).per(6)
   end
 end
