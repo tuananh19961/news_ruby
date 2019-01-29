26a0b9631387db8cb947f5a64602e8e69746a5de
tuananh29081996 <tuananh29081996@gmail.com>
2019-01-29 21:10:11 +0700

    config timezone

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-    @new_posts = Post.eager_load(:category).order('posts.id ASC').last(2);
+    @new_posts = Post.includes(:category).order('posts.id ASC').last(2);
     @posts = Post.all
   end
 end
